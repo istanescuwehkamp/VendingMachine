6c14abb162eeb0afb70e77e12da8adc077fd52fa
Radu Iscu <linkerro@gmail.com>
2016-08-01 19:25:08 +0300

    added support for quantities per choice

--- VendingMachine/UnitTest1.cs
+++ VendingMachine/UnitTest1.cs
@@ -12,13 +12,13 @@ namespace VendingMachine
         {
             var vendingMachine = new VendingMachine();
             var result = vendingMachine.Deliver(Choice.Cola);
-            Assert.AreEqual(null,result);
+            Assert.AreEqual(null, result);
         }
 
         [TestMethod]
         public void ShouldReturnACan()
         {
-            var vendingMachine=new VendingMachine();
+            var vendingMachine = new VendingMachine();
             vendingMachine.AddChoice(Choice.Cola);
             var can = vendingMachine.Deliver(Choice.Cola);
             Assert.IsNotNull(can);
@@ -27,11 +27,21 @@ namespace VendingMachine
         [TestMethod]
         public void ShouldReturnACanOfTheSelectedChoice()
         {
-            var vendingMachine=new VendingMachine();
+            var vendingMachine = new VendingMachine();
             vendingMachine.AddChoice(Choice.Fanta);
             vendingMachine.AddChoice(Choice.Cola);
             var can = vendingMachine.Deliver(Choice.Fanta);
-            Assert.AreEqual(Choice.Fanta,can.Type);
+            Assert.AreEqual(Choice.Fanta, can.Type);
+        }
+
+        [TestMethod]
+        public void ShouldSetTheInventoryCorrectly()
+        {
+            var vendingMachine = new VendingMachine();
+            vendingMachine.AddChoice(Choice.Cola, 1);
+            vendingMachine.Deliver(Choice.Cola);
+            var can = vendingMachine.Deliver(Choice.Cola);
+            Assert.IsNull(can);
         }
     }
 
@@ -43,19 +53,22 @@ namespace VendingMachine
 
     public class VendingMachine
     {
-        private List<Choice> _choices=new List<Choice>();
+        private List<Choice> _choices = new List<Choice>();
+        private Dictionary<Choice, int> _choiceQuantities = new Dictionary<Choice, int>();
 
         public Can Deliver(Choice choice)
         {
-            if (!_choices.Contains(choice))
+            if (!_choices.Contains(choice)||_choiceQuantities[choice]<1)
             {
                 return null;
             }
-            return new Can {Type = choice};
+            _choiceQuantities[choice] -= 1;
+            return new Can { Type = choice };
         }
 
-        public void AddChoice(Choice choice)
+        public void AddChoice(Choice choice, int count = int.MaxValue)
         {
+            _choiceQuantities.Add(choice, count);
             _choices.Add(choice);
         }
     }
